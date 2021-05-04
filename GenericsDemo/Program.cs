aae3dd32e787197ba6ade4d33a4e2751b5096c66
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-04 19:51:23 +0530

    [soham]Class based generics method added

--- GenericsDemo/Program.cs
+++ GenericsDemo/Program.cs
@@ -13,21 +13,21 @@ namespace GenericsDemo
             int[] intArray = { 1, 2, 3, 4, 5 };
             double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
             char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+             
+            //ClassBasedGenerics
 
-            Program.ToPrint(intArray);
-            Program.ToPrint(doubleArray);
-            Program.ToPrint(charArray);
+            PrintArray<int> printArray = new PrintArray<int>(intArray);
+            printArray.ToPrint();
+            Console.WriteLine("--------------------------------------");
+            PrintArray<double> printDouble = new PrintArray<double>(doubleArray);
+            printDouble.ToPrint();
+            Console.WriteLine("--------------------------------------");
+            new PrintArray<int>(intArray).ToPrint(); //easyway
+            Console.WriteLine("--------------------------------------");
+            new PrintArray<char>(charArray).ToPrint();
+
             Console.ReadKey();
         }
 
-        //MethodBased
-        public static void ToPrint<T>(T[] inputArray)
-        {
-            foreach (var element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("----------------------------------");
-        }
     }
 }
