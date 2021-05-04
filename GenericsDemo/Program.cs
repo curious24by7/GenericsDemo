f8f596854d5226498698df41500064bf579a6572
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-04 09:39:13 +0530

    [soham]Added GenericsDemo

---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+            //Program.toPrint(intArray);
+            //Program.toPrint(doubleArray);
+            //Program.toPrint(charArray);
+
+            Program.ToPrint<int>(intArray);
+            Program.ToPrint<double>(doubleArray);
+            Program.ToPrint<char>(charArray);
+            Console.ReadKey();
+        }
+
+        public static void ToPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------------------");
+        }
+
+        //public static void toPrint(int[] inputArray)
+        //{
+        //    foreach (double element in inputArray)
+        //    {
+        //        Console.WriteLine(element);
+        //    }
+        //    Console.WriteLine("----------------------------------");
+        //}
+
+        //public static void toPrint(double[] inputArray)
+        //{
+        //    foreach (double element in inputArray)
+        //    {
+        //        Console.WriteLine(element);
+        //    }
+        //    Console.WriteLine("----------------------------------");
+        //}
+
+        //public static void toPrint(char[] inputArray)
+        //{
+        //    foreach (char element in inputArray)
+        //    {
+        //        Console.WriteLine(element);
+        //    }
+        //    Console.WriteLine("-------------------------------------");
+        //}
+    }
+
+
+}
